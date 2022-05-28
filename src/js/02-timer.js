6eeef864c0ba6902da27f5c82f752c94f30e967e
Yanko Dima <yanko.dmitriy@gmail.com>
2022-05-28 23:35:37 +0300

    fix

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,7 +1,15 @@
 const refs = {
     startBtn: document.querySelector('button[data-start]'),
     days: document.querySelector('.value[data-days]'),
-    hours,
-    minutes,
-    seconds
+    hours: document.querySelector('.value[data-hours]'),
+    minutes: document.querySelector('.value[data-minutes]'),
+    seconds: document.querySelector('.value[data-seconds]'),
 };
+
+refs.startBtn.addEventListener('click', () => {
+    console.log('Start Timer');
+});
+
+// const timer = {
+
+// };
