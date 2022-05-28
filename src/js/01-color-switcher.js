f0b4af4f3020235f01c9ebe47445e83fb3392d9c
Yanko Dima <yanko.dmitriy@gmail.com>
2022-05-28 18:47:09 +0300

    add links

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,12 +1,12 @@
 const refs = {
-    startBtn: document.querySelector('.js-start'),
-    stopBtn: document.querySelector('.js-stop'),
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop'),
 };
 
 refs.startBtn.addEventListener('click', onStartBtnClick);
 refs.stopBtn.addEventListener('click', onStopBtnClick);
 
-console.log('refs.startBtn');
+// console.log(refs.startBtn);
 
 // function getRandomHexColor() {
 //     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
