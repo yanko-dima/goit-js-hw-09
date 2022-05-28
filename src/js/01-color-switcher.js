9d9411d59057378ca903195389df418fba9d44d0
Yanko Dima <yanko.dmitriy@gmail.com>
2022-05-28 22:32:20 +0300

    Do Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,21 +1,44 @@
+let intervalId = null;
 const refs = {
     startBtn: document.querySelector('button[data-start]'),
     stopBtn: document.querySelector('button[data-stop'),
+    body: document.querySelector('body'),
 };
 
 refs.startBtn.addEventListener('click', onStartBtnClick);
 refs.stopBtn.addEventListener('click', onStopBtnClick);
 
-// console.log(refs.startBtn);
-
-// function getRandomHexColor() {
-//     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-//   }
-
 function onStartBtnClick() {
-    console.log('Start');
+    // Дизаблим кнопку старт
+    onDisabledStartBtn()
+    // Если не запущен - запускаем интервал 
+    onCreateIntercal();
 };
 
 function onStopBtnClick() {
-    console.log('Stop');
+    // Останавливаем интервал
+    onClearInterval();
+    // Делаем кнопку Старт активной
+    onActiveStartBtn();
 };
+
+  function onCreateIntercal() {
+      intervalId = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+      }, 1000);
+  };
+  function onClearInterval() {
+      clearInterval(intervalId);
+  };
+
+  function onDisabledStartBtn() {
+    refs.startBtn.disabled = true;
+  };
+
+  function onActiveStartBtn() {
+    refs.startBtn.disabled = false;
+  };
+
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
