73a7c3403891e8a759fd570d7c09e0badb36a6d4
Yanko Dima <yanko.dmitriy@gmail.com>
2022-05-29 17:38:43 +0300

    Timer Do

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,79 +1,80 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
     startBtn: document.querySelector('button[data-start]'),
-    // inputDat: document.getElementById('datetime-picker'),
     days: document.querySelector('.value[data-days]'),
     hours: document.querySelector('.value[data-hours]'),
     minutes: document.querySelector('.value[data-minutes]'),
     seconds: document.querySelector('.value[data-seconds]'),
 };
+let intervalId = null;
+
+refs.startBtn.disabled = true;
+
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+};
+
+function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    // Remaining days
+    const days = addLeadingZero(Math.floor(ms / day));
+    // Remaining hours
+    const hours = addLeadingZero(Math.floor((ms % day) / hour));
+    // Remaining minutes
+    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+    // Remaining seconds
+    const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+  
+    return { days, hours, minutes, seconds };
+};
+  
+const onClose = (selectedDates, dateStr, instance) => {
+    const calcTime = selectedDates[0] - instance.now;
+
+    if(calcTime && calcTime > 0) {
+        refs.startBtn.disabled = false;
+
+        const startTimer = () => {
+            refs.startBtn.disabled = true;
+        
+            // const startTime = Date.now();
+        
+            intervalId = setInterval(() => {
+                const currentTime = Date.now();
+                
+                const deltaTime = selectedDates[0] - currentTime;
+                let timerUpdate = convertMs(deltaTime);
+        
+                // Запись обновления
+                refs.days.innerHTML = timerUpdate.days;
+                refs.hours.innerHTML = timerUpdate.hours;
+                refs.minutes.innerHTML = timerUpdate.minutes;
+                refs.seconds.innerHTML = timerUpdate.seconds;
+              }, 1000);
+         };
+        
+         refs.startBtn.addEventListener('click', startTimer);
+
+    } else {
+        Notify.failure('Please choose a date in the future');
+    };
+
+ }; 
 
 const options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
+    onClose,
   };
-
-// refs.startBtn.disabled = true;
-
-// flatpickr(inputDat, options);
-// console.log(refs.days);
-
-class Timer {
-    constructor({onTick}) {
-        this.isActive = false;
-        this.onTick = onTick;
-    }
-
-    start() {
-        if(this.isActive) {
-            return;
-        }
-
-        const startTime = Date.now();
-        this.isActive = true;
-
-        setInterval(() => {
-            const currentTime = Date.now();
-            const deltaTime = currentTime - startTime;
-            const time = this.getTimeComponents(deltaTime);
-
-            this.onTick(time);
-          }, 1000);
-    }
-
-    getTimeComponents(time) {
-        const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-        const hours = this.pad(Math.floor(time % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)));
-        const minutes = this.pad(Math.floor(time % (1000 * 60 * 60) / (1000 * 60)));
-        const seconds = this.pad(Math.floor(time % (1000 * 60) / (1000)));
-    
-        return { days, hours, minutes, seconds };
-    }
-
-    pad(value) {
-        return String(value).padStart(2, '0');
-    };
-};
-
-const timer = new Timer({
-    onTick: updateClokFace,
-    targetDay: new Date('Jul 17, 2022'),
-});
-
-refs.startBtn.addEventListener('click', timer.start.bind(timer));
-
-function updateClokFace({ days, hours, minutes, seconds }) {
-    refs.days.textContent = days;
-    refs.hours.textContent = hours;
-    refs.minutes.textContent = minutes;
-    refs.seconds.textContent = seconds;
-};
 
 let fp = flatpickr('#datetime-picker', options);
